97fa00e8e7f4af6ee6f5c11e61963040b24ab3b4
samuelassuncao <samuel.coutoassuncao@gmail.com>
2023-01-26 15:58:39 -0300

    Criadas as ferramentas de edição de tarefas

--- Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -11,26 +11,45 @@ namespace Tarefas.Web.Controllers
 
         public TarefaController()
         {
-            listaDeTarefas = new List<Tarefa>()
-            {
-                new Tarefa() { Id = 1, Titulo = "Escovar os dentes" },
-                new Tarefa() { Id = 2, Titulo = "Arrumar a cama" },
-                new Tarefa() { Id = 3, Titulo = "Por o lixo para fora", Descricao = "somente às terças, quintas e sábados" }
-            };
+           
         }
         
         public IActionResult Details(int id)
         {
-            var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
-            return View(tarefa);
-        }
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
 
+            var tarefa = new Tarefa()
+            {
+                Id =tarefaDTO.Id,
+                Titulo = tarefaDTO.Titulo,
+                Descricao = tarefaDTO.Descricao,
+                Concluida = tarefaDTO.Concluida
+            };
+                return View(tarefa);
+        }
+        
         public IActionResult Index()
-        {            
+        {
+            var tarefaDAO= new TarefaDAO();
+            var listaDeTarefasDTO = tarefaDAO.Consultar();
+
+            var listaDeTarefas= new List<Tarefa>();
+
+            foreach (var tarefaDTO in listaDeTarefasDTO)
+            {
+                listaDeTarefas.Add(new Tarefa()
+            {
+                Id=tarefaDTO.Id,
+                Titulo=tarefaDTO.Titulo,
+                Descricao=tarefaDTO.Descricao,
+                Concluida=tarefaDTO.Concluida
+            });
+            }
             return View(listaDeTarefas);
         }
 
-        public IActionResult Create()
+        public IActionResult Create()        
         {
             return View();
         }
@@ -49,6 +68,39 @@ namespace Tarefas.Web.Controllers
             tarefaDAO.Criar(tarefaDTO);
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(Tarefa tarefa)
+        {
+            var tarefaDTO = new TarefaDTO
+            {
+                Id = tarefa.Id,
+                Titulo = tarefa.Titulo,
+                Descricao = tarefa.Descricao,
+                Concluida = tarefa.Concluida
+            };
+    
+            var tarefaDAO = new TarefaDAO();
+            tarefaDAO.Atualizar(tarefaDTO);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
+
+            var tarefa = new Tarefa()
+            {
+                Id =tarefaDTO.Id,
+                Titulo = tarefaDTO.Titulo,
+                Descricao = tarefaDTO.Descricao,
+                Concluida = tarefaDTO.Concluida
+            };
+
+            return View(tarefa);
         }
     }
 }
